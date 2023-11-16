4e11b65bbb6e97853f40101fced7f7c3e8e99edb
nahum <nahum.jrm@gmail.com>
2023-11-16 02:00:13 -0600

    actualizacion header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import userimg from '../imgenes/user.jpg'
 // iconos
-import {RiHome2Line } from "react-icons/ri";
+import {
+  RiHome2Line,
+  RiArchiveDrawerFill,
+  RiCalendar2Fill,
+  RiCustomerService2Fill,
+  RiFolderSettingsFill,
+  RiMenu3Fill,
+  RiCloseFill
+} from "react-icons/ri";
 
 
 const Sidebar = () => {
+  const [showMenu, setshowMenu]= useState(false);
   return (
-    <div className='bg-primary-900 h-full'>
-      {/* perfil */}
-      <div className='flex items-center flex-col justify-center p-8 gap-2 h-[30vh]'>
-        <img src={userimg} className='w-20 h-20 object-cover rounded-full ' />
-        <h1 className=' text-xl text-white font-bold'>Nahúm Medina </h1>
-        <p className=' bg-primary-100 py-1 px-3 text-black  text-center rounded-full'>
-          Administrador
+    <>
+      <div className={`bg-primary-900 h-full fixed lg:static w-[80%] md:[40%] lg:w-full transitoin-all duration-300 ${showMenu ? "left-0 " : "-left-full"}`}>
+        {/* perfil */}
+        <div className='flex items-center flex-col justify-center p-8 gap-2 h-[30vh]'>
+          <img src={userimg} className='w-20 h-20 object-cover rounded-full ' />
+          <h1 className=' text-xl text-white font-bold'>Nahúm Medina </h1>
+          <p className=' bg-primary-100 py-1 px-3 text-black  text-center rounded-full'>
+            Administrador
           </p>
-      </div>
-      
-      {/* nav  */}
-      <div className=' bg-primary-200 p-8 rounded-tr-[100px] h-[70vh] overflow-y-scroll flex flex-col justify-between'>
-      <nav>
-<a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
- transition-colors'>
-<RiHome2Line/> Home
-</a>
-      </nav>
-<div>
-<button
-        className='w-40 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-500	 text-white text-lg text-bold
-        '>
-        Cerrar Sesión
-        </button>
-</div>
-      </div>
+        </div>
 
-    </div>
+        {/* nav  */}
+        <div className=' bg-primary-200 p-8 rounded-tr-[100px] h-[70vh]  flex flex-col justify-between gap-6'>
+          <nav className='flex flex-col gap-8'>
+            <a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
+            transition-colors'>
+              <RiHome2Line /> Home
+            </a>
+            <a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
+            transition-colors'>
+              <RiArchiveDrawerFill /> Solicitudes
+            </a>
+            <a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
+            transition-colors'>
+              <RiCalendar2Fill /> Calendario
+            </a>
+            <a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
+            transition-colors'>
+              <RiFolderSettingsFill /> Procesos
+            </a>
+            <a href='#' className='flex items-center gap-4 text-black py-2 px-4 rounded-xl hover:bg-primary-900/50
+            transition-colors'>
+              <RiCustomerService2Fill /> Servicios
+            </a>
+          </nav>
 
+        </div>
+
+      </div>
+      {/* boton mobile */}
+
+      <button onClick={()=> setshowMenu(!showMenu)}
+      className=' lg:hidden fixed  right-4 bottom-4 text-2xl bg-primary-900 p-2.5 rounded-full text-white'>
+        {showMenu ? <RiCloseFill/> : <RiMenu3Fill/>}
+      </button>
+    </>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,29 @@
-import * as React from 'react'
-import { signOut } from 'firebase/auth'
-import { auth } from '../credenciales'
-import Sidebar from '../components/Sidebar';
+import * as React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../credenciales";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
-
-export default function Home({
-  user,
-  setAuthState,
-  setUser
-}) {
+export default function Home({ user, setAuthState, setUser }) {
   const singOutHandler = () => {
     signOut(auth)
       .then(() => {
         setUser(null);
-        setAuthState('login');
+        setAuthState("login");
       })
       .catch((err) => console.log(err));
-  }
-
-
+  };
 
   return (
-    <div >
-  <div className='grid grid-cols-6 min-h-screen'>
-<Sidebar/>
-<div>
-          <button
-            onClick={singOutHandler}
-            className='w-40 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-500	 text-white text-lg text-bold '>
-            Cerrar Sesión
-          </button>
+    <div>
+      <div className="grid grid-cols-6 min-h-screen">
+        <Sidebar />
+        <div>
+          <main className="lg:col-span-3 xl:col-span-5 p-8">
+            <Header />
+          </main>
         </div>
-  </div>
-
-
+      </div>
     </div>
-    
-  )
-}
-
+  );
+};
